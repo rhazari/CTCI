18355fc171ff04cb8222e5f6f4c0c6c8fcd710e8
John Harvard <jharvard@appliance.localdomain>
2014-09-13 20:15:53 -0400

    new commit

---
+++ Stack_Queues/queue_wstack.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> s1;
+stack<int> s2;
+
+void push(int value)
+{
+	if(s2.empty())
+	{
+		s1.push(value);
+	}
+	else
+	{	
+		while(!s2.empty())
+		{
+			s1.push(s2.top());
+			s2.pop();
+		}
+		s1.push(value);
+	}
+}
+
+void remove()
+{
+	while(!s1.empty())
+	{
+		s2.push(s1.top());
+		s1.pop();
+	}
+	if(!s2.empty())
+	{
+		cout <<"Element removed: "<<s2.top()<<endl;
+		s2.pop();
+	}
+	else
+	{
+		cout<<"Queue is empty"<<endl;
+	}
+}
+
+int main()
+{
+	int ch, num, read = 1;
+	
+	while(read)
+	{
+		cout<<"1- Push"<<endl;
+		cout<<"2- Remove"<<endl;
+		cout<<"Default-Exit"<<endl;
+		cin >> ch;
+		switch(ch)
+		{
+			case 1:
+				cout<<"Enter a value"<<endl;
+				cin >> num;
+				push(num);
+				break;
+			case 2:
+				remove();
+				break;
+			default:
+				read = 0;
+				break;
+		}
+	}
+	
+	return 0;
+}
---
+++ Stack_Queues/sorted_stack.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+	int data;
+	struct node* next;
+}
+node;
+
+void peek(struct node* ptr)
+{
+	while(ptr)
+	{
+		printf("%d ",ptr->data);
+		ptr = ptr->next;
+	}
+	printf("\n");
+}
+
+void push(struct node** head, int value)
+{
+	node* newptr = (node*)malloc(sizeof(node));
+	newptr->data = value;
+	newptr->next = NULL;
+
+	if((*head) == NULL || value < (*head)->data)
+	{
+		newptr->next = (*head);
+		(*head) = newptr;
+	}
+	else
+	{
+		node *ptr, *prev;
+		ptr = (*head);
+		prev = NULL;
+		while(ptr->data < value && ptr->next != NULL)
+		{
+			prev = ptr;
+			ptr = ptr->next;
+		}
+		if(ptr->data < value && ptr->next == NULL)
+		{
+			ptr->next = newptr;
+		}
+		else
+		{
+			prev->next = newptr;
+			newptr->next = ptr;
+		}
+	}
+}
+
+int isEmpty(struct node** head)
+{
+	if((*head)== NULL)
+		return 0;
+	else
+		return 1;
+}
+
+void pop(struct node** head)
+{
+	if(isEmpty(head))
+	{
+		printf("Element popped: %d\n",(*head)->data);
+		node *temp = (*head);
+		(*head) = (*head)->next;
+		free(temp);
+	}
+	else
+	{
+		printf("Stack is empty\n");
+	}
+}
+
+int main()
+{
+	node *head = NULL;
+	push(&head, 11);
+	push(&head, 14);
+	push(&head, 13);
+	push(&head, 9);
+	push(&head, 7);
+	push(&head, 21);
+	push(&head, 17);
+	push(&head, 5);
+	
+	peek(head);
+
+	pop(&head);
+	pop(&head);
+	peek(head);
+
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	pop(&head);
+	return 0;
+}
